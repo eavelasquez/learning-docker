132fadc03bca466194a2ebe39a2af2babedecd93
eavelasquez <eavc.30@gmail.com>
2023-08-07 13:53:04 -0500

    feat: add hello world example

---
+++ examples/hello-world/README.md
@@ -0,0 +1,9 @@
+# Hello World
+
+This is a simple example that runs the [Hello World](https://hub.docker.com/_/hello-world) Docker image.
+
+## Usage
+
+```bash
+./run.sh
+```
---
+++ examples/hello-world/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# This is a simple script that runs the hello-world docker image
+# --rm flag removes the container after it exits
+docker container run --rm hello-world
